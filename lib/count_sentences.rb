55c5db8af6c7970d4e7b9a9e38876bc2ee0542fc
Kat Maldon <katharinamaldon@gmail.com>
2019-04-16 16:05:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = []
+    array << self.split(".", "?", "!") 
+    array.size = self.size
+    self.size
   end
 end
