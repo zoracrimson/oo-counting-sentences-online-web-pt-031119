8d331959284c4e589aa889c6979454d1ca3ff8f3
Kat Maldon <katharinamaldon@gmail.com>
2019-04-16 16:10:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return array.length
   end
 end
